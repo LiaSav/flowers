9eb0736f86dc3d24e0023197fa7575330ef923ea
Lia <liasav19@ukr.net>
2021-06-19 14:06:28 +0300

    slidesPlugin

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const slides = document.querySelectorAll(".slide");
+function slidesPlugin(activeSlide = 2) {
+  const slides = document.querySelectorAll(".slide");
+  slides[activeSlide].classList.add("active");
+  for (const slide of slides) {
+    slide.addEventListener("click", function () {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
+  function clearActiveClasses() {
+    slides.forEach(function (slide) {
+      slide.classList.remove("active");
+    });
+  }
+}
 
-for (const slide of slides) {
-  slide.addEventListener("click", function () {
-    clearActiveClasses();
-    slide.classList.add("active");
-  });
-}
-function clearActiveClasses() {
-  slides.forEach(function (slide) {
-    slide.classList.remove("active");
-  });
-}
+slidesPlugin();
